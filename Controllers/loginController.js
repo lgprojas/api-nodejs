739ce45b86c8f8a05b31014b481e209590af7ef5
Athel-dev <contacto@athel.cl>
2023-07-16 00:30:47 -0400

    Se agrega await a loginController en generateToken

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -23,7 +23,7 @@ const loginUsu = async(req, res) => {
     const { _id, nombre, email} = datosUsu
 
     //aquí podría agregar el token a la session
-    const token = generateToken({id: _id, email: email})
+    const token = await generateToken({id: _id, email: email})
     const datosUsuToken = {nombre, email, token}
 
     res.status(201).send({ status: "OK", data: datosUsuToken });
