95e2cda2927f0911f984e10624e5e8c1c5197e49
Athel-dev <contacto@athel.cl>
2023-07-16 00:25:24 -0400

    Se agrega async await a jwt.js

--- Middleware/jwt.js
+++ Middleware/jwt.js
@@ -2,11 +2,11 @@ const { ObjectId } = require('bson');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const generateToken = ({ id, email }) => {
+const generateToken = async({ id, email }) => {
     const token_key = process.env.TOKEN_KEY;
     const idUser = new ObjectId(id).valueOf()//quita el ObjectId
 
-    const token = jwt.sign(
+    const token = await jwt.sign(
         {userId:idUser, email:email},
         token_key,
         {expiresIn: "2h"}
@@ -14,7 +14,7 @@ const generateToken = ({ id, email }) => {
     return token
 }
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async(req, res, next) => {
     const token_key = process.env.TOKEN_KEY;
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -22,7 +22,7 @@ const verifyToken = (req, res, next) => {
     
     if(token==null)
         return res.status(401).send("Token requerido");
-    jwt.verify(token, token_key, (err, user) => {
+    await jwt.verify(token, token_key, (err, user) => {
         if(err) return res.status(403).send('Token inválido');
         console.log(user);
         req.user = user;
